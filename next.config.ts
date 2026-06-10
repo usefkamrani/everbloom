a4fb1f271842e98d6ff6e6f1c64113938e43b9e7
Usef Kamrani <u3fkmp@gmail.com>
2026-06-10 10:08:48 +0330

    chore(next): disable Next image optimizer (images.unoptimized=true)

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+  devIndicators: false,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
